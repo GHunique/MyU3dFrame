101d1c8a8e0ee70c1bbf1eaf2d2a0a33cf045dc9
GHunique <xiangshouyong@01D-0008.local>
2013-12-26 14:48:03 +0800

    firstCommit

---
+++ FUD/Assets/Editor/LoadingEditor/MyEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(loadingController))]
+public class MyEditor : Editor
+{
+
+	void OnGUI()
+	{
+		loadingController loading = (loadingController)target;
+		loading.mValue =  EditorGUILayout.Slider(loading.mValue,1, 100);
+	}
+
+	public override void OnInspectorGUI()
+	{
+		loadingController loading = (loadingController)target;
+		loading.mValue = EditorGUILayout.Slider("Progress",loading.mValue,0f,1f);
+
+		float increased = EditorGUILayout.FloatField("PerIncrease",loading.mPerAdd);
+		if(increased >= 0 && increased <= 10)
+			loading.mPerAdd = increased;
+
+		//添加触发函数
+		loading.TrigTarget = EditorGUILayout.ObjectField("Target",loading.TrigTarget,typeof(GameObject)) as GameObject;
+		loading.TriggerFunName = EditorGUILayout.TextField("FunctionName",loading.TriggerFunName);
+	}
+}
